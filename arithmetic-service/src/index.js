e7479ada3b24c8c6901e44496c98736fe4ecd4ee
harsh reddy <57232194+harsha-23@users.noreply.github.com>
2024-02-08 15:34:54 -0500

    Create index.js

---
+++ arithmetic-service/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 3000;
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+});
+
+// sample testcase -> http://localhost:3000/add?num1=2&num2=4 => output:6
+// num-app.html for e2e workflow
+app.get('/add', (req, res) => {
+    // Extracting two numbers from query parameters
+    // should integrate this rest endpoint in num-app JS scirpt
+    const num1 = parseFloat(req.query.num1);
+    const num2 = parseFloat(req.query.num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        res.status(400).send('Invalid numbers');
+    } else {
+        const sum = num1 + num2;
+        res.send(`The sum of ${num1} and ${num2} is: ${sum}`);
+    }
+});
+
+app.listen(port);
